bb0ad17c3b6d5a14cd7c9653ecf632c56f30e031
nabende1 <briannabende1@gmail.com>
2024-02-25 18:46:22 +0300

    resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Proud Tech";
+        job1._startYear = 2029;
+        job1._endYear = 2024;
+        // job1.Display();
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+        // job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Brian Nabende";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
